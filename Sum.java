bea562dded966342e2c3c478aee85c67000e1c33
baki-511 <johanbaki381@gmail.com>
2022-12-08 09:06:26 +0530

    initial Commit

---
+++ Sum.java
@@ -0,0 +1,10 @@
+/**
+ * Sum
+ */
+public class Sum {
+    public static void main(String[] args) {
+        int a=10,b=30;
+        int sum = a+b;
+        System.out.println("Sum of two num = "+sum);
+    }
+}

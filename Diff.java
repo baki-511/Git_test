02b1b8422f6459de609cdef59620dd2e41a6c1bf
baki-511 <johanbaki381@gmail.com>
2022-12-08 09:31:25 +0530

    Difference method changed

--- Diff.java
+++ Diff.java
@@ -1,3 +1,6 @@
 public class Diff {
-    
+    public static void main(String[] args) {
+        int a=100,b=50;
+        System.out.println("Difference = "+(a-b));
+    }
 }
---
+++ Multiply.java
@@ -0,0 +1,6 @@
+public class Multiply {
+    public static void main(String[] args) {
+        int a=5,b=6;
+        System.out.println("Multiply = "+(a*b));
+    }
+}
